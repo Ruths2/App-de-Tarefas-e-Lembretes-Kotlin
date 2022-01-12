410b65a66d12303404ec3ba5dac5523d74ce0534
Ruths2 <ruth09@yahoo.com>
2022-01-12 01:36:01 -0300

    Cria projeto Lista de Tarefas e Lembretes

---
+++ Lista de Tarefas e Lembretes/app/src/main/java/br/com/lembretes/tarefas/lista/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.lembretes.tarefas.lista
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_main)
+    }
+}
